af407fbfa087a50bcf99b99c3255eb1e623f618e
Leystryku <Leystryku@gmail.com>
2025-02-06 21:58:54 +0100

    - add hitscan

--- leyhitreg/lua/leyhitreg/server/bulletprocessing/hitscan.lua
+++ leyhitreg/lua/leyhitreg/server/bulletprocessing/hitscan.lua
@@ -10,21 +10,53 @@
 -- the calculation is done in bullet cb to make it less expensive
 -- because most of the time, just picking the bone pos should work
 
---  bullet missed even though it should not have, try hitscan
+-- Bullet missed even though it should not have, try hitscan.
+-- Luckily we can do this via the HullSize, as  we're in bullet cb here
+-- which utilizes TraceHull for bullets if set
+LeyHitreg.RefireHullSize = 100
 function LeyHitreg:HitScan(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos)
-    -- TODO: do hitscan here
+    bullet.HullSize = self.RefireHullSize
+    ply:FireBullets(bullet)
+
+    return true
 end
 
 local IsValid = IsValid
 
-function LeyHitreg:HitScanBulletCallback(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos, cbdata)
-    local traceres = cbdata[2]
+function LeyHitreg:HitScanBulletCallback(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos, atk, traceres, dmginfo)
+    if (bullet.HullSize >= self.RefireHullSize) then
+        return
+    end
 
     if (IsValid(traceres.Entity)) then
         return
     end
 
-    self:HitScan(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos)
+    return self:HitScan(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos)
+end
+
+function LeyHitreg:BulletCallback(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos, oldCallback, ...)
+    local cbdata = {...}
+
+    if (cbdata[1] != ply) then
+        return oldCallback(...)
+    end
+
+    local ignoreBullet = hook.Call("LeyHitreg.OnBulletCallback", cbdata[1], cbdata[2], cbdata[3])
+
+    if (ignoreBullet) then
+        return
+    end
+
+    if (LeyHitreg.HitScanDisabled) then
+        return oldCallback(...)
+    end
+
+    if (LeyHitreg:HitScanBulletCallback(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos, cbdata[1], cbdata[2], cbdata[3])) then
+        return
+    end
+
+    return oldCallback(...)
 end
 
 function LeyHitreg:HitScanBullet(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos)
@@ -35,26 +67,6 @@ function LeyHitreg:HitScanBullet(ply, target, bullet, shot, targetBone, targetHi
     local oldCallback = bullet.Callback or function() end
 
     bullet.Callback = function(...)
-        local cbdata = {...}
-
-        if (cbdata[1] != ply) then
-            return oldCallback(...)
-        end
-
-        local ignoreBullet = hook.Call("LeyHitreg.OnBulletCallback", cbdata[1], cbdata[2], cbdata[3])
-
-        if (ignoreBullet) then
-            return
-        end
-
-        if (LeyHitreg.HitScanDisabled) then
-            return oldCallback(...)
-        end
-
-        if (LeyHitreg:HitScanBulletCallback(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos, cbdata)) then
-            return
-        end
-
-        return oldCallback(...)
+        return self:BulletCallback(ply, target, bullet, shot, targetBone, targetHitGroup, shootpos, oldCallback, ...)
     end
 end
